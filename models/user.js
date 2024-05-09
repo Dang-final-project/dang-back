a376bfbc34bc23dc11c16cae47a8a1aea161508b
gigajini <bh5322@naver.com>
2024-05-09 15:11:56 +0900

    유저테이블생성 및 회원가입 구현

---
+++ models/user.js
@@ -0,0 +1,41 @@
+const Sequelize = require("sequelize");
+
+class User extends Sequelize.Model {
+    static initiate(sequelize) {
+        User.init({
+            email: {
+                type: Sequelize.STRING(40),
+                allowNull: true,
+                unique: true,
+            },
+            username: {
+                type: Sequelize.STRING(15),
+                allowNull: false,
+            },
+            password: {
+                type: Sequelize.STRING(100),
+                allowNull: true,
+            },
+            provider: {
+                type: Sequelize.ENUM("local", "kakao"),
+                allowNull: false,
+                defaultValue: "local",
+            },
+            kakaoId: {
+                type: Sequelize.STRING(30),
+                allowNull: true,
+            },
+            refreshToken: {
+                type: Sequelize.STRING(255),
+                allowNull: true,
+            }
+        },{
+            sequelize,
+            paranoid: true,
+            charset: "utf8",
+            collate: "utf8_general_ci",
+        });
+    }
+}
+
+module.exports = User;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const { join } = require('../controllers/auth');
+
+// POST/V1/auth/join
+router.post('/auth/join', join);
 
 
 module.exports = router;
