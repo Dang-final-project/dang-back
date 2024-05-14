d15be5f55a1d929982b0a70b90283ac5eb13e123
kueunsu <sss5732@naver.com>
2024-05-14 17:40:18 +0900

    kakao

--- passport/index.js
+++ passport/index.js
@@ -1,5 +1,5 @@
 const passport = require("passport")
-// const { User } = require("../models")
+const { User } = require("../models")
 const local = require("./local");
 const jwt = require("./jwt");
 const kakao = require("./kakao");
@@ -13,5 +13,14 @@ module.exports = () => {
     // 전략 성공 시 호출
     passport.serializeUser((user, done) => {
         done(null, user.id)
+    })
+    passport.deserializeUser((id, done) => {
+        User.findOne({
+            where: { id },
+        })
+        .then(user => {
+            done(null, user);
+        })
+        .catch(err => done(err));
     })
 }
