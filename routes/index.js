0ae627e429bc1cf61f343a6734e414a4764b2993
gigajini <bh5322@naver.com>
2024-05-17 11:14:18 +0900

    즐겨찾기,메모 헤더 수정

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const { join } = require('../controllers/auth');
 const stationRouter = require('./station');
 const authRouter = require('./auth');
+const { verifyToken } = require('../middlewares');
 
 // v1/stations
-router.use('/stations', stationRouter);
+router.use('/stations', verifyToken, stationRouter);
 
 router.use('/auth', authRouter);
 
--- routes/station.js
+++ routes/station.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { addStation, getStation, deleteStation, writeMemo } = require('../controllers/station');
 
 //즐겨찾기 조회(/v1/stations/list)
-router.post('/list', getStation);
+router.get('/list', getStation);
 
 //즐겨찾기 등록(/v1/stations/add)
 router.post('/add',addStation);
